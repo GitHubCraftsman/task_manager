e83a1ecb4b1e720d814acb4618dabecc837a986f
GitHubCraftsman <mobilezozulya@gmail.com>
2020-04-09 20:34:01 +0300

    sqlite3 fixed for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-config.action_mailer.default_url_options = { host: 'https://afternoon-peak-58950.herokuapp.com', port: 3000 }
+config.action_mailer.default_url_options = { host: 'https://afternoon-peak-58950.herokuapp.com', port: 5000 }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
